66390d084454ad9a65fa1944b0148a3412555dae
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-11-18 23:14:38 -0500

    Create my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,30 +1,12 @@
 module Enumerable
-  def my_any?
-    i = 1
-    self.each do |element|
-        if yield(element) == true
-          return true
-        elsif i >= self.length
-          return false 
-        end
-      i += 1  
-      end
-  end
-end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
-class Array
-  # Define my_each here
-  def my_each
-    hold = []
+  def my_each_with_index
+    i = 0
     self.each do |array|
-      hold << array if yield(array)
-      
+      yield(array, i) if block_given?
+      i += 1  
     end
-    return self #after not finding anything and iterating all the array
+    return self
   end
 
   def my_select
@@ -54,6 +36,36 @@ class Array
       return false 
     end
   end
+
+  def my_any?
+    i = 1
+    self.each do |element|
+        if yield(element) == true
+          return true
+        elsif i >= self.length
+          return false 
+        end
+      i += 1  
+      end
+  end
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each
+    hold = []
+    self.each do |array|
+      hold << array if yield(array)
+      
+    end
+    return self #after not finding anything and iterating all the array
+  end
+
+  
 end
 
 
